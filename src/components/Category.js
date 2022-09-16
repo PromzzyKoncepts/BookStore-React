33a70103ea55c1d490049f355a59af0ae830b5ef
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-16 19:20:28 +0100

    Add button for category component

---
+++ src/components/Category.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Update = {
+  backgroundColor: '#0290FF',
+  padding: '15px 20px',
+  border: 'none',
+  color: '#fff',
+  borderRadius: '4px',
+  cursor: 'pointer',
+};
+
+const Category = () => (
+  <>
+    <div style={{ margin: 'auto' }}>
+      <button type='button' style={Update}>Check Status</button>
+    </div>
+  </>
+);
+export default Category;
