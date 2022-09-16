1725f1e2b40c7678742f76da6f391ace4f971b28
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-16 19:20:56 +0100

    Check for linters

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 // import AddBooks from './components/BookContainer';
 import BookList from './components/BookList';
 import NavBar from './components/NavBar';
+import Category from './components/Category';
 
 const App = () => (
   <div className="wrapper">
     <NavBar />
     <Routes>
       <Route path="/" exact element={<BookList />} />
+      <Route path="/category" exact element={<Category />} />
     </Routes>
   </div>
 );
