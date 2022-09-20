de4f6547838a10bec9999ef982caae184f08bf15
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-20 21:36:16 +0100

    Update file to display UI corrctly

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './style.css';
-// import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-// import { createStore } from 'redux'
-// import rootReducer from './reducers'
 import App from './App';
-
-// const store = createStore(rootReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
