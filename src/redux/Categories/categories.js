2d901d3a1e81e0ea3e17694d42f572a373d2e86f
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-09-20 21:34:00 +0100

    Update file to contain reducers

--- src/redux/Categories/categories.js
+++ src/redux/Categories/categories.js
@@ -1,36 +1,13 @@
-const STATUS = 'BOOKSTORE-REACT/category/STATUS';
-const ADD_CATEGORY = 'BOOKSTORE-REACT/categories/ADD_CATEGORY';
-const REMOVE_CATEGORY = 'BOOKSTORE-REACT/categories/REMOVE_CATEGORY';
-
+const CHECK_STATUS = 'BOOKSTORE-REACT/category/CHECK_STATUS';
 
 // Reducer Function
-const checkStatus = (state = false, action) => {
+const checkStatus = (state = [], action) => {
   switch (action.type) {
-    case STATUS:
-      return !state;
-    case ADD_CATEGORY:
-      return [...state, action.book];
-
-    case REMOVE_CATEGORY:
-      return state.filter((book) => book !== action.bookId);
-
+    case CHECK_STATUS:
+      return 'Under Construction';
     default:
       return state;
   }
 };
-
-// Add A Book Category action
-
-export const addBook = (book) => ({
-  type: 'ADD_CATEGORY',
-  book,
-});
-
-// Remove a book Category action
-
-export const removeBook = (bookId) => ({
-  type: 'REMOVE_CATEGORY',
-  bookId,
-});
 
 export default checkStatus;
